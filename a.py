7eef76a927455770bc33e1b86f0298d3f432ad01
MUHAMMED YAZEEN <27795050+messenger-1012@users.noreply.github.com>
2020-07-23 22:55:45 +0530

    Create a.py

---
+++ a.py
@@ -0,0 +1,21 @@
+s=""
+test_list = [] 
+rev=[]
+alpha = 'a'
+beta='z'
+for i in range(0, 26): 
+    test_list.append(alpha) 
+    alpha = chr(ord(alpha) + 1)
+    rev.append(beta)
+    beta = chr(ord(beta) - 1)
+   
+
+a=input().lower()
+for i in a:
+    if i in test_list:
+        k=(test_list.index(i))
+        s=s+rev[k]
+    else:
+        s=s+i
+print(s)            
+
